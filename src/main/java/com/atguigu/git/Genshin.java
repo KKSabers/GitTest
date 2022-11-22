d7277bcaec02a74aa34b35d6249d055658eb2f13
kuanglonglong <kuanglonglong0835@foxmail.com>
2022-11-22 23:57:02 +0800

    测试用...

---
+++ src/main/java/com/atguigu/git/Genshin.java
@@ -0,0 +1,18 @@
+package com.atguigu.git;
+
+/**
+ * @Author: Simples
+ * @Description: TODO
+ * @Date: 2022/11/22
+ * @Version: 1.0
+ */
+public class Genshin {
+    public static void main(String[] args) {
+        System.out.println("GenShin is best Game !!!");
+        System.out.println("GenShin is best Game !!!");
+        System.out.println("GenShin is best Game !!!");
+        System.out.println("GenShin is best Game !!!");
+        System.out.println("GenShin is best Game !!!");
+        System.out.println("GenShin is best Game !!!");
+    }
+}
